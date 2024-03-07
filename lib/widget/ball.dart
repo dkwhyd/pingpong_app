7a080c5d215169c8bde048e6aec7eb661af0085c
dkwhyd <dikiwhyd99@gmail.com>
2024-03-07 12:03:55 +0700

    fixing

--- lib/widget/ball.dart
+++ lib/widget/ball.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Ball extends StatelessWidget {
+  const Ball({super.key});
   @override
   Widget build(BuildContext context) {
-    final double diameter = 50;
+    const double diameter = 50;
 
     return Container(
       width: diameter,
       height: diameter,
-      decoration: new BoxDecoration(
+      decoration: BoxDecoration(
         color: Colors.amber[400],
         shape: BoxShape.circle,
       ),
