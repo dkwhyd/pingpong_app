63ec2d571fa5e447c12f1babe474e0e66639948c
dkwhyd <dikiwhyd99@gmail.com>
2024-03-10 21:53:46 +0700

    save highscore in pref

--- lib/widget/pong.dart
+++ lib/widget/pong.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:pingpong/widget/ball.dart';
 import 'package:pingpong/widget/bat.dart';
@@ -30,8 +30,10 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   Direction hDir = Direction.right;
 
   double speed = 5;
-
   int score = 0;
+  SharedPreferences? prefs;
+  int? highScore;
+
   @override
   void initState() {
     super.initState();
@@ -42,6 +44,8 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
     );
     animation = Tween<double>(begin: 0, end: 100).animate(controller!);
     animation!.addListener(() {
+      readHighScore();
+
       setState(() {
         (hDir == Direction.right ? posX += speed : posX -= speed);
         (vDir == Direction.down ? posY += speed : posY -= speed);
@@ -67,8 +71,8 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
               right: 1,
               child: Column(
                 children: [
-                  const Text('Score :'),
-                  Text(score.toString())
+                  Text('Score :$score'),
+                  Text('High score:${highScore.toString()} ')
                 ],
               ),
             ),
@@ -138,7 +142,7 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
           score += 1;
         });
       } else {
-        resetScore();
+        setHighScore(score);
         controller!.stop();
         gameOver(context);
         // dispose();
@@ -191,23 +195,34 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
       posY = 0;
       speed = 5;
     });
+    resetScore();
   }
 
   void gameOver(BuildContext context) {
+    TextStyle textStyle = TextStyle(fontSize: 20);
     showDialog(
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
           return AlertDialog(
             title: const Center(
-              child: Text('Game Over'),
+              child: Text(
+                'Game Over',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
             ),
             content: SizedBox(
-              height: 40,
+              height: 50,
               child: Column(
                 children: [
-                  Text('Score :$score'),
-                  const Text('High score: '),
+                  Text(
+                    'Score :$score',
+                    style: textStyle,
+                  ),
+                  // Text(
+                  //   'High score: ${highScore}',
+                  //   style: textStyle,
+                  // ),
                 ],
               ),
             ),
@@ -221,5 +236,16 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
             ],
           );
         });
+  }
+
+  void readHighScore() async {
+    prefs = await SharedPreferences.getInstance();
+    highScore = prefs!.getInt('highScore');
+  }
+
+  void setHighScore(int currentScore) {
+    if (highScore! < currentScore) {
+      prefs!.setInt('highScore', score);
+    }
   }
 }
